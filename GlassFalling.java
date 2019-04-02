c5757019185778b5de4642e839e16cfef3e52582
karina12345 <32123761+karina12345@users.noreply.github.com>
2019-04-02 19:24:53 -0400

    Update GlassFalling.java

--- GlassFalling.java
+++ GlassFalling.java
@@ -23,12 +23,45 @@ public class GlassFalling {
 	       return minimum; // minimum trials
   }
 
-  // Optional:
+ 
   // Pick whatever parameters you want to, just make sure to return an int.
-  public int glassFallingMemoized() {
-    // Fill in here and change the return
-    return 0;
+  public int glassFallingMemoized(int floors, int sheets) {
+    int[][] table = new int[sheets+1][floors+1];
+		// store results in a look up table
+		
+		for (int sheet = 0; sheet <= sheets; sheet++) {
+			for (int floor = 0; floor <= floors; floor++){
+				table[sheet][floor] = Integer.MAX_VALUE;	
+			}
+		}
+		return gfMemo(floors, sheets, table);  
   }
+  	// helper method
+	public int gfMemo(int floors, int sheets, int trials[][]) {
+		// base cases
+		if(floors == 0) return 0;
+		if(floors == 1) return 1;
+		if(sheets == 1) return floors;
+		 
+		 int minimum = floors; 
+		 
+		 for(int i, j, k = 1; k < floors; k++) {
+			 i = trials[sheets-1][k-1]; // glass breaks
+			 j = trials[sheets][floors-k]; // doesn't break
+			 if(i == Integer.MAX_VALUE) {
+				i = gfMemo(k-1, sheets-1, trials);
+			 }
+			 if(j == Integer.MAX_VALUE) {  
+				j = gfMemo(floors-k, sheets, trials);
+			 }
+			 minimum = Math.min(minimum, Math.max(i, j));	 
+	          }
+			 
+		  if(floors < trials[sheets][floors]) {
+	             trials[sheets][floors] = minimum + 1;
+		  } 
+		  return minimum + 1; // number of trials
+          }
 
   // Do not change the parameters!
   public int glassFallingBottomUp(int floors, int sheets) {
@@ -58,7 +91,7 @@ public class GlassFalling {
 	        		                  trials[floor-x][sheet]); 
 	          trials[floor][sheet] = Math.min(outcome, trials[floor][sheet]);
 	        }
-	    }
+	     }
       }
       return trials[floors][sheets]; // minimum number of trials
   }
